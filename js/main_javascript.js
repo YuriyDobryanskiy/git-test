488a0aad87cdcbe46d510bb922c4cdc302a0bd63
Yuriy <juriydobryansky@gmail.com>
2020-04-21 01:47:06 +0300

    commit on master #2

--- js/main_javascript.js
+++ js/main_javascript.js
@@ -1,8 +1,5 @@
 <script>
 $("body").css({"background-color":"grey"});
-<<<<<<< HEAD
-$("wrapper").css({"color":"black"});
-=======
-$(".test").css({"background-color":"green"});
->>>>>>> masterV2
+$("test").css({"background-color":"black"});
+
 </script>
